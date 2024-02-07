a4274af7c5b41000b1e4a4590780ce3195120f48
ric-amorim <116798130+ric-amorim@users.noreply.github.com>
2024-02-07 23:51:32 +0000

    change webcam.cpp to only show the webcam

--- src/webcam.cpp
+++ src/webcam.cpp
@@ -14,7 +14,7 @@ int main(){
     cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
     for(;;){
         // Capture next frame
-        cv::Mat frame, frame_gray , frame_can;
+        cv::Mat frame;
         cap >> frame;
 
         // Check if frame was captured successfully
@@ -22,20 +22,8 @@ int main(){
             std::cout << "Error reading frame from webcam." << std::endl;
             break;
         }
-        cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-        cv::GaussianBlur(frame_gray, frame_gray, cv::Size(5, 5), 0);
-        cv::Canny(frame_gray, frame_can, 50, 150);
-
-        std::vector<std::vector<cv::Point> > contours;
-        cv::findContours(frame_can, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
-
-        cv::Mat contourImage(frame_can.size(), CV_8UC3, cv::Scalar(0,0,0));
-        cv::Scalar color(255,0,0);
-        for(size_t i=0; i<contours.size(); i++){
-            cv::drawContours(contourImage, contours, i, color);
-        }
         // Display frame
-        cv::imshow("Webcam", contourImage);
+        cv::imshow("Webcam", frame);
     
         // Wait 10 mms and check if ESC was pressed
         if(cv::waitKey(10) == 27){
