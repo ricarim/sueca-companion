cc81a6ba532fc93a65fd4519db1ed2992f304bb6
ric-amorim <116798130+ric-amorim@users.noreply.github.com>
2024-03-25 10:33:34 +0000

    remove react-native for now

--- react-native-firebase/utils/useAppNavigation.tsx
+++
@@ -1,6 +0,0 @@
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { RootStackParamList } from '../navigation';
-
-export const useAppNavigation = () => {
-    return useNavigation<NavigationProp<RootStackParamList>>();
-};
